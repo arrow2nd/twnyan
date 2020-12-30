f4fe3628a55928c28f9d23563391e359c4f3dbcb
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2020-12-30 17:04:58 +0900

    add: 処理実行時の確認を追加

--- cmd/config.go
+++ cmd/config.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/arrow2nd/twnyan/config"
+	"github.com/arrow2nd/twnyan/util"
 	"gopkg.in/abiosoft/ishell.v2"
 )
 
@@ -28,7 +29,10 @@ func init() {
 		Help:     "regenerate the configuration file",
 		LongHelp: "Regenerate the configuration file.\nRedo the application authentication.",
 		Func: func(c *ishell.Context) {
-			config.Setup()
+			// 実行確認
+			if util.ExecConfirmation("All settings will be initialized. Are you sure?", "Initialization canceled") {
+				config.Setup()
+			}
 		},
 	})
 
