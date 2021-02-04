05fb803c6588d5adba94f6c3a65d4e16a8634bf1
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-02-04 12:29:23 +0900

    remove: quoteコマンドを削除

--- cmd/retweet.go
+++ cmd/retweet.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"net/url"
-
 	"gopkg.in/abiosoft/ishell.v2"
 )
 
@@ -23,19 +21,6 @@ func (cmd *Cmd) newRetweetCmd() {
 	}
 
 	rc.AddCmd(&ishell.Cmd{
-		Name:    "quote",
-		Aliases: []string{"qt"},
-		Func:    cmd.quoteCmd,
-		Help:    "quote a tweet",
-		LongHelp: createLongHelp(
-			"Quote a tweet.\nIf there is no tweet text, 'にゃーん' will be posted.\nIf you are submitting an image, please add the file name separated by a space.",
-			"qt",
-			"retweet quote [<tweetnumber>] [text] [image]...",
-			"retweet quote 0 cute!! cat.png",
-		),
-	})
-
-	rc.AddCmd(&ishell.Cmd{
 		Name:    "remove",
 		Aliases: []string{"rm"},
 		Func: func(c *ishell.Context) {
@@ -51,41 +36,4 @@ func (cmd *Cmd) newRetweetCmd() {
 	})
 
 	cmd.shell.AddCmd(rc)
-}
-
-func (cmd *Cmd) quoteCmd(c *ishell.Context) {
-	val := url.Values{}
-	// 引数をチェック
-	if len(c.Args) < 1 {
-		cmd.drawWrongArgMessage(c.Cmd.Name)
-		return
-	}
-
-	// 引用するツイートのURLを取得
-	uri, err := cmd.view.GetTweetURL(c.Args[0])
-	if err != nil {
-		cmd.drawErrorMessage(err.Error())
-		return
-	}
-
-	// 引数をパース
-	status, files := cmd.parseTweetCmdArgs(c.Args[1:])
-	// 画像をアップロード
-	if len(files) != 0 {
-		mediaIDs, err := cmd.upload(files)
-		if err != nil {
-			cmd.drawErrorMessage(err.Error())
-			return
-		}
-		val.Add("media_ids", mediaIDs)
-	}
-
-	// URLを追加してツイート
-	status += " " + uri
-	tweetStr, err := cmd.api.PostTweet(val, status)
-	if err != nil {
-		cmd.drawErrorMessage(err.Error())
-		return
-	}
-	cmd.drawMessage("QUOTED", tweetStr, cmd.cfg.Color.Retweet)
 }
