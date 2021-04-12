270b9b696b6d67673a0cc84e2b274399f8fa5b0e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-04-12 11:08:51 +0900

    clean: 変数名を変更

--- view/tweets.go
+++ view/tweets.go
@@ -19,18 +19,18 @@ func (v *View) ShowRegisteredTweets() {
 
 // ShowTweetsFromArray 配列からツイートを表示
 func (v *View) ShowTweetsFromArray(tweets []anaconda.Tweet, shouldShowTweetNum bool) {
-	tagStr := " "
+	tagText := " "
 
 	for i := len(tweets) - 1; i >= 0; i-- {
 		if shouldShowTweetNum {
-			tagStr = fmt.Sprintf(" %d ", i)
+			tagText = fmt.Sprintf(" %d ", i)
 		}
-		v.ShowTweet(&tweets[i], tagStr, false)
+		v.ShowTweet(&tweets[i], tagText, false)
 	}
 }
 
 // ShowTweet ツイートを表示
-func (v *View) ShowTweet(tweets *anaconda.Tweet, tagStr string, isQuote bool) {
+func (v *View) ShowTweet(tweets *anaconda.Tweet, tagText string, isQuote bool) {
 	header := ""
 	halfWidth := util.GetWindowWidth() / 2
 
@@ -49,7 +49,7 @@ func (v *View) ShowTweet(tweets *anaconda.Tweet, tagStr string, isQuote bool) {
 	}
 
 	// ヘッダー文字列を作成
-	tagStr = color.HEXStyle(v.cfg.Color.BoxForground, v.cfg.Color.Accent1).Sprint(tagStr)
+	tagStr := color.HEXStyle(v.cfg.Color.BoxForground, v.cfg.Color.Accent1).Sprint(tagText)
 	userInfoStr := v.createUserInfoString(&tweets.User)
 	createdAt, _ := tweets.CreatedAtTime()
 	createdAtStr := v.createCreatedAtString(createdAt)
@@ -71,29 +71,28 @@ func (v *View) ShowTweet(tweets *anaconda.Tweet, tagStr string, isQuote bool) {
 // editTweetText ツイート文を編集
 func (v *View) editTweetText(tweet *anaconda.Tweet) string {
 	// 文字をアンエスケープ
-	text := html.UnescapeString(tweet.FullText)
-	text += "\n"
+	tweetText := html.UnescapeString(tweet.FullText)
+	tweetText += "\n"
 
 	// ハッシュタグをハイライト
 	if len(tweet.Entities.Hashtags) != 0 {
 		for _, h := range tweet.Entities.Hashtags {
 			rep := regexp.MustCompile(fmt.Sprintf(`[#＃](%s)([\s　])`, h.Text))
-			text = rep.ReplaceAllString(text, color.HEX(v.cfg.Color.Hashtag).Sprintf("#$1$2"))
+			tweetText = rep.ReplaceAllString(tweetText, color.HEX(v.cfg.Color.Hashtag).Sprintf("#$1$2"))
 		}
 	}
 
 	// メンションをハイライト
 	if len(tweet.Entities.User_mentions) != 0 {
 		rep := regexp.MustCompile(`(^|[^\w@#$%&])[@＠](\w+)`)
-		text = rep.ReplaceAllString(text, "$1"+color.HEX(v.cfg.Color.Reply).Sprintf("@$2"))
+		tweetText = rep.ReplaceAllString(tweetText, "$1"+color.HEX(v.cfg.Color.Reply).Sprintf("@$2"))
 	}
 
-	return text
+	return tweetText
 }
 
 // createCountString いいね・RT数の文字列を作成
 func (v *View) createCountString(countNum int, reverseFlg bool, unitStr string) string {
-	// 表示色
 	colorCode := v.cfg.Color.Favorite
 	if unitStr == "RT" {
 		colorCode = v.cfg.Color.Retweet
@@ -106,15 +105,12 @@ func (v *View) createCountString(countNum int, reverseFlg bool, unitStr string) 
 		unitStr += "s"
 	}
 
-	// 文字列作成
-	text := " "
+	// フラグが立っていれば文字を反転する
 	if reverseFlg {
-		text += color.HEXStyle(v.cfg.Color.BoxForground, colorCode).Sprintf(" %d%s ", countNum, unitStr)
+		return color.HEXStyle(v.cfg.Color.BoxForground, colorCode).Sprintf(" %d%s ", countNum, unitStr)
 	} else {
-		text += color.HEX(colorCode).Sprintf("%d%s", countNum, unitStr)
+		return color.HEX(colorCode).Sprintf("%d%s", countNum, unitStr)
 	}
-
-	return text
 }
 
 // RegisterTweets ツイートを登録
@@ -128,14 +124,14 @@ func (v *View) RegisterTweets(tweets *[]anaconda.Tweet) {
 
 // GetTweetURL ツイートのURLを取得
 func (v *View) GetTweetURL(tweetNumStr string) (string, error) {
-	name, err := v.GetDataFromTweetNum(tweetNumStr, "screenname")
+	screenname, err := v.GetDataFromTweetNum(tweetNumStr, "screenname")
 	if err != nil {
 		return "", err
 	}
 
-	ID, _ := v.GetDataFromTweetNum(tweetNumStr, "tweetID")
+	tweetID, _ := v.GetDataFromTweetNum(tweetNumStr, "tweetID")
 
-	return fmt.Sprintf("https://twitter.com/%s/status/%s", name, ID), nil
+	return fmt.Sprintf("https://twitter.com/%s/status/%s", screenname, tweetID), nil
 }
 
 // GetDataFromTweetNum ツイート番号から情報を取得
