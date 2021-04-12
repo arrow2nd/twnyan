ed49585319a8f17511f1e82d697e697108335084
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-04-12 20:52:40 +0900

    update: バージョン

--- cmd/version.go
+++ cmd/version.go
@@ -4,16 +4,15 @@ import (
 	"github.com/arrow2nd/ishell"
 )
 
-const ver = "1.3.0"
+const versionStr = "1.4.0"
 
 func (c *Cmd) addVersionCmd() {
 	c.shell.AddCmd(&ishell.Cmd{
 		Name:    "version",
 		Aliases: []string{"ver"},
 		Func: func(c *ishell.Context) {
-			c.Printf("twnyan🐾 ver.%s\n", ver)
+			c.Printf("twnyan🐾 ver.%s\n", versionStr)
 		},
-		Help:     "Displays the version",
-		LongHelp: "",
+		Help: "display version",
 	})
 }
