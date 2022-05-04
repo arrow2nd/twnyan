bd8dd90c9567a88bd1fedbbded33cb7427be0aaf
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-04 21:34:00 +0900

    fix(view): 一部ハッシュタグが正しくハイライトされない

--- view/tweet.go
+++ view/tweet.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"html"
 	"regexp"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/arrow2nd/twnyan/util"
@@ -96,21 +98,55 @@ func (v *View) ShowTweet(tweets *anaconda.Tweet, tagText string, isQuote bool) {
 // processTweetText ツイート文を加工
 func (v *View) processTweetText(tweet *anaconda.Tweet) string {
 	// 文字をアンエスケープ
-	tweetText := html.UnescapeString(tweet.FullText) + "\n"
+	text := html.UnescapeString(tweet.FullText) + "\n"
 
 	// ハッシュタグをハイライト
 	if len(tweet.Entities.Hashtags) != 0 {
-		rep := regexp.MustCompile(`[#＃](\S+)`)
-		tweetText = rep.ReplaceAllString(tweetText, color.HEX(v.config.Color.Hashtag).Sprintf("#$1"))
+		text = v.highlightHashTags(text, &tweet.Entities)
 	}
 
 	// メンションをハイライト
 	if len(tweet.Entities.User_mentions) != 0 {
 		rep := regexp.MustCompile(`(^|[^\w@#$%&])[@＠](\w+)`)
-		tweetText = rep.ReplaceAllString(tweetText, "$1"+color.HEX(v.config.Color.Reply).Sprintf("@$2"))
+		text = rep.ReplaceAllString(text, "$1"+color.HEX(v.config.Color.Reply).Sprintf("@$2"))
 	}
 
-	return tweetText
+	return text
+}
+
+// highlightHashTags ハッシュタグをハイライト
+func (v *View) highlightHashTags(text string, entities *anaconda.Entities) string {
+	result := ""
+	runes := []rune(text)
+	endPos := 0
+
+	for _, hashtag := range entities.Hashtags {
+		// ハッシュタグの開始位置 ("#"を含まない)
+		beginPos := hashtag.Indices[0] + 1
+
+		// NOTE: APIから帰ってくる開始位置が間違っている(値が大きすぎる)場合があるので
+		//       正しい位置が見つかるまで、開始位置を前方に移動させる
+		//       終了条件が i > 0 なので、beginPos は "#" を含むハッシュタグの開始位置になる
+		for ; beginPos > 0; beginPos-- {
+			if i := strings.Index(string(runes[beginPos:]), hashtag.Text); i > 0 {
+				break
+			}
+		}
+
+		// 前方の文とハイライトされたハッシュタグを結合
+		hashtagText := fmt.Sprintf("#%s", hashtag.Text)
+		result += string(runes[endPos:beginPos]) + color.HEX(v.config.Color.Hashtag).Sprint(hashtagText)
+
+		// ハッシュタグの終了位置
+		endPos = beginPos + utf8.RuneCountInString(hashtagText)
+	}
+
+	// 残りの文を結合
+	if len(runes) > endPos {
+		result += string(runes[endPos:])
+	}
+
+	return result
 }
 
 // createCountString いいね・RT数の文字列を作成
